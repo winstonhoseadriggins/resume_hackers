9a1041b100255f4bc5ecb6c7f6d5e7a670ca4690
Shawn Patrick Doyle <shawnpatrickdoyle@gmail.com>
2015-10-09 14:11:01 -0400

    Mandrill working

--- main.rb
+++ main.rb
@@ -1,4 +1,6 @@
 require 'sinatra'
+require 'mandrill'
+
 
 get '/' do 
 	@title = "home"
@@ -12,15 +14,33 @@ get '/services' do
 	@title = "services"
 	erb :services
 end	
-get '/products' do
-	@title = "products"
-	erb :products	
-end
+
 get '/form' do
-	@title = "our form"
+	@title = "sign_up"
 	erb :form
 end
+
 post '/signin' do
-	@title = "signed in"
-	erb :signin
+	puts params.inspect
+	user_name = params["name"]
+	user_email = params["email"]
+	user_message = params["message"]
+	api_key = ENV['MANDRILL_APIKEY']
+	m = Mandrill::API.new
+	message = {  
+ 	:subject=> "your resume_hacker boarding pass",  
+ 	:from_name=> "resume_hacker",  
+ 	:text=> user_message,  
+ 	:to=>[  
+   {  
+     :email=> user_email,  
+     :name=> user_name 
+   }  
+ 	],  
+ 	:html=>user_message,  
+ 	:from_email=>"shawnpatrickdoyle@gmail.com"  
+	}  
+	sending = m.messages.send message  
+puts sending
+erb :signin
 end
